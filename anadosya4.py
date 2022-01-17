4cd3b65709a36f0f1d8a7c8b2f381bb6999afcf5
bulentsezen <50150555+bulentsezen@users.noreply.github.com>
2022-01-17 16:52:44 +0300

    Add files via upload

---
+++ anadosya4.py
@@ -0,0 +1,47 @@
+import cv2
+
+
+# Opencv DNN
+net = cv2.dnn.readNet("dnn_model/yolov4-tiny.weights", "dnn_model/yolov4-tiny.cfg")
+model = cv2.dnn_DetectionModel(net)
+model.setInputParams(size=(320, 320), scale=1/255)
+
+
+# Load class lists
+classes = []
+with open("dnn_model/classes.txt", "r") as file_object:
+    for class_name in file_object.readlines():
+        #print(class_name)
+        class_name = class_name.strip()  # satır arası boşluklar için
+
+        classes.append(class_name)
+
+
+
+# Initialize camera
+cap = cv2.VideoCapture(0)
+
+
+while True:
+    # Get frames
+    ret, frame = cap.read()
+
+
+    # Object Detection
+    (class_ids, scores, bboxes) = model.detect(frame, confThreshold=0.3, nmsThreshold=.4)
+    for class_id, score, bbox in zip(class_ids, scores, bboxes):
+        (x, y, w, h) = bbox
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (200,0,50), 3)
+
+        class_name = classes[class_id]
+
+        cv2.putText(frame, class_name, (x, y - 10), cv2.FONT_HERSHEY_PLAIN, 3, (200,0,50), 2)
+
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
