54105652a02e93315eaa79b62602d27cb44494ee
bulentsezen <50150555+bulentsezen@users.noreply.github.com>
2023-01-22 14:16:12 +0300

    Add files via upload

---
+++ object_tracking - 1.py
@@ -0,0 +1,55 @@
+import cv2
+import numpy as np
+from object_detection import ObjectDetection
+import math
+
+# Initialize Object Detection
+od = ObjectDetection()
+
+#cap = cv2.VideoCapture("los_angeles.mp4")
+cap = cv2.VideoCapture("ucak1.mp4")
+#cap = cv2.VideoCapture("yangin1.mp4")
+
+# Initialize count
+count = 0
+center_points_prev_frame = []
+
+tracking_objects = {}
+track_id = 0
+
+while True:
+    ret, frame = cap.read()
+    count += 1
+    if not ret:
+        break
+
+    # Point current frame
+    center_points_cur_frame = []
+
+    # Detect objects on frame
+    (class_ids, scores, boxes) = od.detect(frame)
+    print(class_ids)
+    for box in boxes:
+        (x, y, w, h) = box
+        cx = int((x + x + w) / 2)
+        cy = int((y + y + h) / 2)
+        center_points_cur_frame.append((cx, cy))
+        #print("FRAME N°", count, " ", x, y, w, h)
+
+        # cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+
+    for object_id, pt in tracking_objects.items():
+        cv2.circle(frame, pt, 5, (0, 0, 255), -1)
+        cv2.putText(frame, str(object_id), (pt[0], pt[1] - 7), 0, 1, (0, 0, 255), 2)
+
+
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
